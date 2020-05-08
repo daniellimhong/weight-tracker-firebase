d7b627449fc0bb7309e650a4dffb4a9ea0817a80
Daniel <daniellimhong@gmail.com>
2020-05-07 17:31:04 -0700

    Updates

---
+++ precache-manifest.844da29c905d704a813d444af8a795a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9f5d938c9d03334416b88a140f8f01",
+    "url": "/weight-tracker-firebase/index.html"
+  },
+  {
+    "revision": "e7b1f4e933f5a333a51c",
+    "url": "/weight-tracker-firebase/static/css/main.a6980862.chunk.css"
+  },
+  {
+    "revision": "a465732d3863e8a040e2",
+    "url": "/weight-tracker-firebase/static/js/2.3eb49fc7.chunk.js"
+  },
+  {
+    "revision": "27f4e9d907c9233ebaf4894bd38c7354",
+    "url": "/weight-tracker-firebase/static/js/2.3eb49fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b1f4e933f5a333a51c",
+    "url": "/weight-tracker-firebase/static/js/main.fcb8ca8c.chunk.js"
+  },
+  {
+    "revision": "e98b6a3d2b525a0181cb",
+    "url": "/weight-tracker-firebase/static/js/runtime-main.1047b368.js"
+  }
+]);
